bf18af2481d56ad0c054e0f027007216fc26c0f0
Stardust2021 <pradeep.fiziks@gmail.com>
2022-07-01 15:50:52 +0530

    newupload

---
+++ Solution (5).cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<double,double> pd;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+
+const int MOD = 1000000007;
+const double PI = 4*atan(1);
+const ll INF = 1e18;
+const int MX = 100001;
+
+int main() {
+    int T; cin >> T;
+    F0R(i,T) {
+        cout << "Case #" << (i+1) << ": ";
+        int D,N; cin >> D >> N;
+        double ti = 0;
+        F0R(j,N) {
+            int K,S; cin >> K >> S;
+            ti = max(ti,double(D-K)/S);
+        }
+        cout << fixed << setprecision(8) << D/ti << "\n";
+    }
+}
+
+// read the question correctly (is y a vowel?)
+// look out for SPECIAL CASES (n=1?) and overflow (ll vs int?)
